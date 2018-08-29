241c89300adbe9f0204d6e343e20f84151ada08c
Eder <francoheder@gmail.com>
2018-08-28 22:03:23 -0300

    Seção 3: Importando uma biblioteca em JS

--- Typescript/dist/app.js
+++ Typescript/dist/app.js
@@ -2,6 +2,8 @@
 exports.__esModule = true;
 var base_ships_1 = require("./base-ships");
 var startfighter_1 = require("./startfighter");
+var _ = require("lodash");
+console.log(_.pad("Typescript Examples", 40, "="));
 var ship = new base_ships_1.Spacecraft('hyperdrive');
 ship.jumpIntoHyperspace();
 var falcon = new startfighter_1.MillenniumFalcon();
