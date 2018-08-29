08ed1408f5dd27217c8a7e09a900d0d640786c01
Eder <francoheder@gmail.com>
2018-08-28 21:45:45 -0300

    Seção 3: Organizando os exemplos em módulos

--- Typescript/app.ts
+++ Typescript/app.ts
@@ -1,39 +1,11 @@
-class Spacecraft {
-    constructor (public propulsor: string){}
-
-    jumpIntoHyperspace() {
-        console.log(`Entering hyperspace with ${this.propulsor}`)
-    }
-}
+import {Spacecraft, Containership} from './base-ships'
+import {MillenniumFalcon} from './startfighter'
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
-class MillenniumFalcon extends Spacecraft implements Containership {
-    
-    cargoContainer: number
-
-    constructor(){
-        super('hyperdrive')
-        this.cargoContainer = 4
-    }
-
-    jumpIntoHyperspace(){
-        if (Math.random() >= 0.5) {
-            super.jumpIntoHyperspace()
-        }else{
-            console.log('Failed to jump into hyperspace')
-        }
-    }
-}
-
 let falcon = new MillenniumFalcon()
 falcon.jumpIntoHyperspace()
 
-interface Containership {
-    cargoContainer: number
-}
-
 let goodForTheJob = (ship: Containership) => ship.cargoContainer > 2
-
 console.log(`Is falcon good for the job? ${goodForTheJob(falcon) ? 'Yes' : 'No'}`)
